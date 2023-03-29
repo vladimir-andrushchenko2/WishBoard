53de35e34eefb935d5319989a7fa5abe3f5d764b
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-03-29 17:31:52 +0200

    changed api in allowed cors

--- backend/middlewares/corsHandling.js
+++ backend/middlewares/corsHandling.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://bigchungus.nomoredomains.work',
-  'http://bigchungus.nomoredomains.work',
+  'https://api.bigchungus.nomoredomains.work',
+  'http://api.bigchungus.nomoredomains.work',
   'localhost:3000',
 ];
 
