142eb23337b0d63bca0389d64a2c499806c28749
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-03-29 22:28:33 +0200

    changed api for production

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,2 @@
-// export const url = 'http://api.bigchungus.nomoredomains.work'
-export const url = 'http://localhost:8080'
+export const url = 'http://api.bigchungus.nomoredomains.work'
+// export const url = 'http://localhost:8080'
