f61d1d3093308afef6f2c9bf5cf871b0be27e8a0
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-03-29 17:34:06 +0200

    another change of url in cors handling, removed api. prefix this time

--- backend/middlewares/corsHandling.js
+++ backend/middlewares/corsHandling.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://api.bigchungus.nomoredomains.work',
-  'http://api.bigchungus.nomoredomains.work',
+  'https://bigchungus.nomoredomains.work',
+  'http://bigchungus.nomoredomains.work',
   'localhost:3000',
 ];
 
