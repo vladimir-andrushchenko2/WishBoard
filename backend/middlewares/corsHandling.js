365011cb50c94a76c58fe422aad14d4988401367
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-03-29 23:14:54 +0200

    Revert "fixed servers allowed cors"
    This reverts commit 03787f4aa08926d76e1e47b691d1b6a71f4fcc1f.

--- backend/middlewares/corsHandling.js
+++ backend/middlewares/corsHandling.js
@@ -1,8 +1,8 @@
 const { InvalidRequest } = require('../customErrors/InvalidRequest');
 
 const allowedCors = [
-  'https://api.bigchungus.nomoredomains.work',
-  'http://api.bigchungus.nomoredomains.work',
+  'https://bigchungus.nomoredomains.work',
+  'http://bigchungus.nomoredomains.work',
   'http://localhost:3000',
 ];
 
