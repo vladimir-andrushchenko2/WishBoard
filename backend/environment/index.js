0d4debdb55c2ea0b9c31f685b57984951a1c429c
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-03-30 00:07:36 +0200

    added check for env name

--- backend/environment/index.js
+++ backend/environment/index.js
@@ -1,5 +1,9 @@
 const constants = require('../constants');
 
-const JWT_SECRET = process.env.JWT_SECRET ?? constants.DEV_JWT;
+const JWT_SECRET = process.env.NODE_ENV === 'production'
+  ? process.env.JWT_SECRET
+  : constants.DEV_JWT;
+
+console.log(process.env.NODE_ENV ?? 'environment is not specified, running dev');
 
 module.exports = { JWT_SECRET, ...constants };
