6485ada8fbb2d132d2e7f3485069e1b6f43c1483
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-03-30 12:45:56 +0200

    added new error

--- backend/customErrors/index.js
+++ backend/customErrors/index.js
@@ -2,7 +2,12 @@ const { NotFound } = require('./NotFound');
 const { InvalidRequest } = require('./InvalidRequest');
 const { UnauthorizedError } = require('./UnauthorizedError');
 const { DuplicateError } = require('./DuplicateError');
+const { ForbiddenError } = require('./ForbiddenError');
 
 module.exports = {
-  NotFound, InvalidRequest, UnauthorizedError, DuplicateError,
+  NotFound,
+  InvalidRequest,
+  UnauthorizedError,
+  DuplicateError,
+  ForbiddenError,
 };
