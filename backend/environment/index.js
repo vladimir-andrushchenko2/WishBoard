76beccbe880e855f46601ede959a40bda269b43c
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-03 21:19:30 +0200

    moved from cra to vite

--- backend/environment/index.js
+++ backend/environment/index.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.NODE_ENV === 'production'
   ? process.env.JWT_SECRET
   : constants.DEV_JWT;
 
-const { CLIENT_NAME = 'http://localhost:3000' } = process.env;
+const { CLIENT_NAME = 'http://localhost:5173' } = process.env;
 
 console.log(process.env.NODE_ENV ?? 'environment is not specified, running dev');
 console.log(`host name ${CLIENT_NAME}`);
